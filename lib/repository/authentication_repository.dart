e2574ec0ecab90abeb924212274b2573eecb4cf4
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2025-01-10 13:00:34 +0800

    add base authrepository

---
+++ lib/repository/authentication_repository.dart
@@ -0,0 +1,32 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+// TODO: add exception handling
+class AuthenticationRepository {
+  final SupabaseClient _supabase;
+
+  AuthenticationRepository(this._supabase);
+
+  Future<AuthResponse> signUpNewUser(String email, String password) async {
+    final AuthResponse res = await _supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+
+    return res;
+  }
+
+  Future<AuthResponse> signInWithEmail(String email, String password) async {
+    final AuthResponse res = await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+
+    return res;
+  }
+
+  Future<void> signOut() async {
+      await _supabase.auth.signOut();
+    }
+
+}
