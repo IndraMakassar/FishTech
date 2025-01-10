20bd9cfe1dd0103cfb57ae4cd2256a3e99bec648
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2025-01-10 21:19:06 +0800

    move url and anonkey to const

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fishtech/const.dart';
 import 'package:fishtech/router_config.dart';
 import 'package:fishtech/theme.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +6,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
-    url: 'https://tyumikqftrhxgcziakay.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dW1pa3FmdHJoeGdjemlha2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMjM1MDEsImV4cCI6MjA1MTc5OTUwMX0.-12NMKqL9KGkSg-8xlOVpowNf34dFnioZIofPqq7jjs',
+    url: SUPABASE_URL,
+    anonKey: ANON_KEY,
   );
 
   runApp(const MyApp());
