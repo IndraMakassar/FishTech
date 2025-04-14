6a75915de74fe485a7fe8f84ad25621902273795
Akechi001 <enricoarianthous@gmail.com>
2025-04-14 22:27:41 +0800

    Make addPond_screen.dart||pond_model.dart and modify custom_button.dart and custom_text_form_field.dart

--- lib/model/pond_model.dart
+++ lib/model/pond_model.dart
@@ -2,30 +2,73 @@ import 'package:equatable/equatable.dart';
 
 class PondModel extends Equatable {
   final String id;
-  final DateTime createdAt;
   final String name;
+  final String fish;
+  final DateTime createdAt;
+  final double length;
+  final double width;
+  final double height;
+  final double volume;
+
+
 
   const PondModel({
     required this.id,
-    required this.createdAt,
     required this.name,
+    required this.fish,
+    required this.createdAt,
+    required this.length,
+    required this.width,
+    required this.height,
+    required this.volume,
+
   });
 
   @override
-  List<Object?> get props => [id, createdAt, name];
+  List<Object?> get props => [id, createdAt, name, fish, length, width, height, volume];
 
   factory PondModel.fromJson(Map<String, dynamic> data) {
     return PondModel(
       id: data["id"],
-      createdAt: DateTime.parse(data["created_at"]),
       name: data["name"],
+      fish: data["fish"],
+      createdAt: DateTime.parse(data["created_at"]),
+      length: data["length"],
+      width: data["width"],
+      height: data["height"],
+      volume: data["volume"],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       "name": name,
+      "fish":fish,
+      "length":length,
+      "width":width,
+      "height":height,
+      "volume":volume,
     };
   }
-
+  PondModel copyWith({
+    String? id,
+    String? name,
+    String? fish,
+    DateTime? createdAt,
+    double? length,
+    double? width,
+    double? height,
+    double? volume
+  }){
+    return PondModel(
+      id: id??this.id,
+      name: name??this.name,
+      fish: fish??this.fish,
+      createdAt: createdAt?? this.createdAt,
+      length: length?? this.length,
+      width: width?? this.width,
+      height: height??this.height,
+      volume: volume??this.volume
+    );
+  }
 }
