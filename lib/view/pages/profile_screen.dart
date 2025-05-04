2d21a79d79b47759dc803e5090b4c31bc53ad1a2
Antonius Indra <anindhpra@gmail.com>
2025-05-04 21:13:04 +0800

    add logout button in profile

--- lib/view/pages/profile_screen.dart
+++ lib/view/pages/profile_screen.dart
@@ -14,9 +14,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   void initState() {
     super.initState();
-    final state = context
-        .read<AuthBloc>()
-        .state;
+    final state = context.read<AuthBloc>().state;
     if (state is AuthSuccess) {
       _nameController.text = state.session.user.userMetadata!['Display name'];
       _emailController.text = state.session.user.email!;
@@ -85,7 +83,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               text: "Save",
                               onPressed: () {
                                 if (state is AuthSuccess) {
-                                  if (_nameController.text != state.session.user.userMetadata!['Display name']) {
+                                  if (_nameController.text !=
+                                      state.session.user
+                                          .userMetadata!['Display name']) {
                                     context.read<AuthBloc>().add(UserChangeName(
                                         newName: _nameController.text.trim()));
                                   }
@@ -94,6 +94,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             ),
                           ],
                         ),
+                      ),
+                      const Gap(30),
+                      CustomButton(
+                        text: 'Logout',
+                        backgroundColor: Theme.of(context).colorScheme.error,
+                        onPressed: () {
+                          context.read<AuthBloc>().add(UserSignOut());
+                        },
                       ),
                     ],
                   ),
