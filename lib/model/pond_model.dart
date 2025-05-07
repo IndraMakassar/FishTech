6bf131d68000735ad84d61fec73c57d9bfa61f35
Antonius Indra <anindhpra@gmail.com>
2025-05-07 19:53:03 +0800

    updating pond model

--- lib/model/pond_model.dart
+++ lib/model/pond_model.dart
@@ -3,72 +3,50 @@ import 'package:equatable/equatable.dart';
 class PondModel extends Equatable {
   final String id;
   final String name;
-  final String fish;
+  final String? fish;
   final DateTime createdAt;
-  final double length;
-  final double width;
-  final double height;
-  final double volume;
-
-
+  final double? volume;
 
   const PondModel({
     required this.id,
     required this.name,
     required this.fish,
     required this.createdAt,
-    required this.length,
-    required this.width,
-    required this.height,
     required this.volume,
-
   });
 
   @override
-  List<Object?> get props => [id, createdAt, name, fish, length, width, height, volume];
+  List<Object?> get props => [id, createdAt, name, fish, volume];
 
   factory PondModel.fromJson(Map<String, dynamic> data) {
     return PondModel(
       id: data["id"],
-      name: data["name"],
+      name: data["name"] ?? '',
       fish: data["fish"],
       createdAt: DateTime.parse(data["created_at"]),
-      length: data["length"],
-      width: data["width"],
-      height: data["height"],
-      volume: data["volume"],
+      volume: (data["volume"] as num?)?.toDouble(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       "name": name,
-      "fish":fish,
-      "length":length,
-      "width":width,
-      "height":height,
-      "volume":volume,
+      "fish": fish,
+      "volume": volume,
     };
   }
-  PondModel copyWith({
-    String? id,
-    String? name,
-    String? fish,
-    DateTime? createdAt,
-    double? length,
-    double? width,
-    double? height,
-    double? volume
-  }){
+
+  PondModel copyWith(
+      {String? id,
+      String? name,
+      String? fish,
+      DateTime? createdAt,
+      double? volume}) {
     return PondModel(
-      id: id??this.id,
-      name: name??this.name,
-      fish: fish??this.fish,
-      createdAt: createdAt?? this.createdAt,
-      length: length?? this.length,
-      width: width?? this.width,
-      height: height??this.height,
-      volume: volume??this.volume
-    );
+        id: id ?? this.id,
+        name: name ?? this.name,
+        fish: fish ?? this.fish,
+        createdAt: createdAt ?? this.createdAt,
+        volume: volume ?? this.volume);
   }
 }
