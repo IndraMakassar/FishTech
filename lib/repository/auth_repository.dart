6920d3234554c773a7164555a88ddc8abe75078c
Akechi001 <115684730+akechi001@users.noreply.github.com>
2025-05-29 13:15:17 +0800

    Refactor UI and improve error logging in home and pond widgets.
    Enhanced FCM token error logging to include stack traces for better debugging. Refactored home screen UI, optimizing scrollable content and aligning widget styles. Improved pond widgets with better responsiveness and readability, including refined text overflow handling and flexible layouts.

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -20,16 +20,15 @@ class AuthRepository {
       .asBroadcastStream();
 
 
-
   AuthRepository(this._supabase);
 
   Stream<AuthStateChange> get onAuthStateChange => _authChanges;
 
   Future<AuthResponse> signUpWithEmail(
-    String name,
-    String email,
-    String password,
-  ) async {
+      String name,
+      String email,
+      String password,
+      ) async {
     final AuthResponse res = await _supabase.auth.signUp(
       email: email,
       password: password,
@@ -66,18 +65,19 @@ class AuthRepository {
   }
 
   Future<void> changeToken(String token) async {
-    final user = _supabase.auth.currentUser;
-    if (user == null) throw Exception('User not authenticated');
+    try {
+      final user = _supabase.auth.currentUser;
+      if (user == null) throw Exception('User not authenticated');
 
-    final response = await _supabase
-        .from('profiles')
-        .upsert({
-          'id': user.id,  // Required for upsert
-          'fcm_token': token,
-        });
-
-    if (response.error != null) {
-      throw response.error!;
+      await _supabase
+          .from('profiles')
+          .upsert({
+        'id': user.id,
+        'fcm_token': token,
+      });
+    } catch (e) {
+      print('Error in changeToken: $e');
+      rethrow;
     }
   }
 }
