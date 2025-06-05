aedb582861aa55ff4e2ac77409bddfff0346e134
Antonius Indra <anindhpra@gmail.com>
2025-06-05 21:06:03 +0800

    fixing static code test

--- lib/model/notification_model.dart
+++ lib/model/notification_model.dart
@@ -4,26 +4,26 @@ class NotifModel extends Equatable{
   final String id;
   final String title;
   final String body;
-  final DateTime created_at;
+  final DateTime createdAt;
   final String status;
 
-  NotifModel({
+  const NotifModel({
     required this.id,
     required this.title,
     required this.body,
-    required this.created_at,
+    required this.createdAt,
     required this.status
   });
 
   @override
-  List<Object?> get props => [title, body, created_at, status];
+  List<Object?> get props => [title, body, createdAt, status];
 
   factory NotifModel.fromJson(Map<String, dynamic> data) {
   return NotifModel(
     id: data["id"]?.toString() ?? '', // Convert to string and provide default
     title: data["title"]?.toString() ?? 'No Title',
     body: data["body"]?.toString() ?? 'No Content',
-    created_at: data["created_at"] != null
+    createdAt: data["created_at"] != null
       ? DateTime.parse(data["created_at"].toString())
       : DateTime.now(),
     status: data["status"]?.toString() ?? 'unread'
